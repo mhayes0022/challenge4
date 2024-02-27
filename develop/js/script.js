82f1989b0db7dde813a4899780421483115ff6a3
MHayes <mhayes0022@gmail.com>
2024-02-26 22:35:24 -0600

    organized high scores

--- develop/js/script.js
+++ develop/js/script.js
@@ -34,73 +34,73 @@ const quizQuestions = [
     A: 'An object with an else method',
     B: 'An object with a then method',
     C: 'A method with a then object',
-    D:  'An object with an if method',
+    D: 'An object with an if method',
     correct: 'An object with a then method',
   },
   // {
-    //   question: 'To style an element directly in the HTML file, what is used?',
-    //   A: 'CSS',
-    //   B: 'CSS Box Model',
-    //   C: 'Internal CSS',
-    //   D: 'Inline CSS',
-    //   correct: 'Inline CSS',
-    // },
-    // {
-      //   question: 'Which Git command stages your changes?',
-      //   A: 'git checkout -b',
-      //   B: 'git add -A',
-      //   C: 'git add -a',
-      //   D: 'git pull origin main',
-      //   correct: 'git add -A',
-      // },
-      // {
-        //   question: 'In CSS, the margin indicates:',
-        //   A: 'How much space we want around the inside of an element',
-        //   B: 'How much space we want around the outside of an element',
-    //   C: 'How much space we want to the top and bottom of an element',
-    //   D: 'How much space we want to the outside of the right and left of an element',
-    //   correct: 'How much space we want around the outside of an element',
-    // },
-    // {
-      //   question: 'What is the selector for an id attribute in a CSS stylesheet?',
-      //   A: '#',
-      //   B: '.idName',
-      //   C: '.className',
-      //   D: 'idAttribute',
-      //   correct: '#',
-      // },
-      // {
-        //   question: 'Which of the following is NOT a valid kind of type?',
-        //   A: 'String',
-        //   B: 'Boolean',
-        //   C: 'Undefined',
-        //   D: 'Element',
-        //   correct: 'Element',
-        // },
-        // {
-          //   question: 'What is an array?',
-          //   A: 'Multiple values stored in a single element',
-          //   B: 'A value stored within a variable',
-          //   C: 'Multiple values stored in a single variable',
-          //   D: 'A value stored across multiple variables',
-          //   correct: 'Multiple values stored in a single variable',
-          // },
-          // {
-            //     question: 'What is a function?',
-            //     A: 'A reusable block of code that performs a specific task',
-            //     B: 'A reusable block of code that does nothing',
-            //     C: 'A one-use-only block of code that performs a specific task',
-            //     D: 'A reusable block of code',
-            //     correct: 'A reusable block of code that performs a specific task',
-            // },
-            // {
-              //     question: 'A variable must be declared in which type of scope for it to be available to all functions?',
-              //     A: 'Universal scope',
-              //     B: 'Local scope',
-              //     C: 'Global scope',
-              //     D: 'Open scope',
-              //     correct: 'Global scope',
-              //   },
+  //   question: 'To style an element directly in the HTML file, what is used?',
+  //   A: 'CSS',
+  //   B: 'CSS Box Model',
+  //   C: 'Internal CSS',
+  //   D: 'Inline CSS',
+  //   correct: 'Inline CSS',
+  // },
+  // {
+  //   question: 'Which Git command stages your changes?',
+  //   A: 'git checkout -b',
+  //   B: 'git add -A',
+  //   C: 'git add -a',
+  //   D: 'git pull origin main',
+  //   correct: 'git add -A',
+  // },
+  // {
+  //   question: 'In CSS, the margin indicates:',
+  //   A: 'How much space we want around the inside of an element',
+  //   B: 'How much space we want around the outside of an element',
+  //   C: 'How much space we want to the top and bottom of an element',
+  //   D: 'How much space we want to the outside of the right and left of an element',
+  //   correct: 'How much space we want around the outside of an element',
+  // },
+  // {
+  //   question: 'What is the selector for an id attribute in a CSS stylesheet?',
+  //   A: '#',
+  //   B: '.idName',
+  //   C: '.className',
+  //   D: 'idAttribute',
+  //   correct: '#',
+  // },
+  // {
+  //   question: 'Which of the following is NOT a valid kind of type?',
+  //   A: 'String',
+  //   B: 'Boolean',
+  //   C: 'Undefined',
+  //   D: 'Element',
+  //   correct: 'Element',
+  // },
+  // {
+  //   question: 'What is an array?',
+  //   A: 'Multiple values stored in a single element',
+  //   B: 'A value stored within a variable',
+  //   C: 'Multiple values stored in a single variable',
+  //   D: 'A value stored across multiple variables',
+  //   correct: 'Multiple values stored in a single variable',
+  // },
+  // {
+  //     question: 'What is a function?',
+  //     A: 'A reusable block of code that performs a specific task',
+  //     B: 'A reusable block of code that does nothing',
+  //     C: 'A one-use-only block of code that performs a specific task',
+  //     D: 'A reusable block of code',
+  //     correct: 'A reusable block of code that performs a specific task',
+  // },
+  // {
+  //     question: 'A variable must be declared in which type of scope for it to be available to all functions?',
+  //     A: 'Universal scope',
+  //     B: 'Local scope',
+  //     C: 'Global scope',
+  //     D: 'Open scope',
+  //     correct: 'Global scope',
+  //   },
 ];
 
 
@@ -122,7 +122,7 @@ function countdown() {
 
 
 //Below enables functionality for the 'start quiz' button
-startBtn.addEventListener('click', function(){
+startBtn.addEventListener('click', function () {
   countdown();
   allRandomQuestions = shuffleQuestions(quizQuestions);
   quizSection.classList.remove('hide');
@@ -149,7 +149,7 @@ function shuffleQuestions(array) {
 
 //This function asks the questions
 function askQuestion() {
-  
+
   let currentQuestion = allRandomQuestions[index];
   if (index >= quizQuestions.length) {
     quizSection.classList.add('hide');
@@ -160,7 +160,7 @@ function askQuestion() {
     playerTime.textContent = 'Final Time:' + ' ' + timeLeft + ' ' + 'seconds!';
     return
   };
-  
+
   document.getElementById('questions').innerHTML = currentQuestion.question;
   document.getElementById('answerA').innerHTML = currentQuestion.A;
   document.getElementById('answerB').innerHTML = currentQuestion.B;
@@ -171,49 +171,49 @@ function askQuestion() {
 
 
 // These enable functionality for the answer buttons in the quiz
-A.addEventListener('click', function(){
-  if(allRandomQuestions[index].A === allRandomQuestions[index].correct) {
+A.addEventListener('click', function () {
+  if (allRandomQuestions[index].A === allRandomQuestions[index].correct) {
     index++;
     askQuestion();
   }
   else {
-    timeLeft-=5;
-    index++;
-    askQuestion();
-  }
-});
-
-B.addEventListener('click', function(){
-  if(allRandomQuestions[index].B === allRandomQuestions[index].correct) {
-    index++;
-    askQuestion()
-  }
-  else {
-    timeLeft-=5;
+    timeLeft -= 5;
     index++;
     askQuestion();
   }
 });
 
-C.addEventListener('click', function(){
-  if(allRandomQuestions[index].C === allRandomQuestions[index].correct) {
+B.addEventListener('click', function () {
+  if (allRandomQuestions[index].B === allRandomQuestions[index].correct) {
     index++;
     askQuestion()
   }
   else {
-    timeLeft-=5;
+    timeLeft -= 5;
     index++;
     askQuestion();
   }
 });
 
-D.addEventListener('click', function(){
-  if(allRandomQuestions[index].D === allRandomQuestions[index].correct) {
+C.addEventListener('click', function () {
+  if (allRandomQuestions[index].C === allRandomQuestions[index].correct) {
     index++;
     askQuestion()
   }
   else {
-    timeLeft-=5;
+    timeLeft -= 5;
+    index++;
+    askQuestion();
+  }
+});
+
+D.addEventListener('click', function () {
+  if (allRandomQuestions[index].D === allRandomQuestions[index].correct) {
+    index++;
+    askQuestion()
+  }
+  else {
+    timeLeft -= 5;
     index++;
     askQuestion();
   }
@@ -225,7 +225,7 @@ D.addEventListener('click', function(){
 let highScoreSection = document.getElementById('highScoreSection');
 let highScoresBtn = document.getElementById('highScoresBtn');
 let submittedScore = document.getElementById('highScoreInitials');
-let highscoresContainer =  document.getElementById('scoresList')
+let highscoresContainer = document.getElementById('scoresList')
 let highScoreInitials = document.getElementById('highScoreInitials');
 let submitScoreBtn = document.getElementById('submitScoreBtn');
 let playAgain = document.getElementById('playAgain');
@@ -234,7 +234,7 @@ let finalTime;
 
 
 //Below enables 'high score' button functionality
-highScoresBtn.addEventListener('click', function(){
+highScoresBtn.addEventListener('click', function () {
   document.getElementById("highScoresBtn").disabled = true;
   quizSection.classList.remove('flex');
   quizSection.classList.add('hide');
@@ -245,52 +245,53 @@ highScoresBtn.addEventListener('click', function(){
   restartBtn.classList.remove('hide');
   highScoreSection.classList.add('marginTop');
   clearInterval(timeInterval);
-  moveScores();
+  displayScores();
 });
 
 
 //Below is the restart quiz button
-restartBtn.addEventListener('click', function(){
+restartBtn.addEventListener('click', function () {
   highScoreSection.classList.add('hide');
   menu.classList.remove('hide');
   restartBtn.classList.add('hide');
-  location.reload(); 
+  location.reload();
 });
 
 
 //Below handles retrieving the high scores
-let saveHighScores = JSON.parse(localStorage.getItem("highScoreSection")) || [];
+let savedHighScores = JSON.parse(localStorage.getItem("highScoreSection")) || [];
 
-const moveScores = () => {
-  saveHighScores.forEach((item) => {
+//looping over high score items and creates list items
+const displayScores = () => {
+  savedHighScores.sort((a, b) => b.score - a.score);
+  savedHighScores.forEach((item) => {
     const initials = document.createElement("li");
     initials.innerHTML = item.initials + ': ' + item.score;
     highscoresContainer.appendChild(initials);
-    
   });
 };
 
 //Below enables functionality of 'submit' score button
 submitScoreBtn.addEventListener('submit', (event) => {
   event.preventDefault();
-    if (submittedScore.value == "" || submittedScore.value == null) {
-      alert("You have not entered your initials");
-      return;
-    }
-    const userScore = {
-      initials : submittedScore.value, 
-      score : timeLeft,
-    }
-    
-    saveHighScores.push(userScore);
-    
-    localStorage.setItem('highScoreSection', JSON.stringify(saveHighScores));
-    moveScores()
-    highScoreSection.classList.remove('hide');
-    highScoreSection.classList.add('marginTop');
-    scoreSubmission.classList.add('hide');
-    menu.classList.add('hide');
-    restartBtn.classList.remove('hide');
+  if (submittedScore.value == "" || submittedScore.value == null) {
+    alert("You have not entered your initials");
+    return;
+  }
+  const userScore = {
+    initials: submittedScore.value,
+    score: timeLeft,
+  }
+
+  savedHighScores.push(userScore);
+
+  localStorage.setItem('highScoreSection', JSON.stringify(savedHighScores));
+  displayScores()
+  highScoreSection.classList.remove('hide');
+  highScoreSection.classList.add('marginTop');
+  scoreSubmission.classList.add('hide');
+  menu.classList.add('hide');
+  restartBtn.classList.remove('hide');
 });
 
 //Below handles if timer runs out
@@ -302,11 +303,11 @@ function youDied() {
   dead.classList.remove('hide');
 };
 
-playAgain.addEventListener('click', function(){
+playAgain.addEventListener('click', function () {
   menu.classList.remove('hide');
   playAgain.classList.add('hide');
   dead.classList.add('hide');
-  location.reload(); 
+  location.reload();
 });
 
 
